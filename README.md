bf65d7ab7e618e4216a4d12aa11e8dd4f07d0160
Corbyn <corbynsal@gmail.com>
2013-04-20 02:47:29 -0500

    Initial commit of code repo

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 AngularTabs
 ===========
 
-Beefed up version of the tabs created on Angular JS homepage tutorial: www.angularjs.org
+Beefed up version of the tabs created on Angular JS homepage tutorial: [www.angularjs.org](http://www.angularjs.org).
+
+* Provides support to ng-views and works correctly with browser back buttons
+
+* Requires: [AngularJS](http://www.angularjs.org) and [bootstrap.min.css](http://twitter.github.io/bootstrap/) to work properly
+
+* To Use:  Declare as it is declared in the tutorial.  Make sure each pane title matches the desired route.  For example, a pane with the title "Home" will route to the corresponding route "#/Home".
---
+++ tabs.js
@@ -0,0 +1,62 @@
+
+angular.module('Spirits').
+directive('tabs', function() {
+	return {
+		restrict: 'E',
+		transclude: true,
+		scope: {},
+		controller: function($scope, $rootScope, $location) {
+			var panes = $scope.panes = [];
+
+			$scope.select = function(pane) {
+				angular.forEach(panes, function(pane) {
+					pane.selected = false;
+				});
+				pane.selected = true;
+			}
+
+			this.addPane = function(pane) {
+				if (panes.length == 0) $scope.select(pane);
+				panes.push(pane);
+			}
+
+            $rootScope.$on('$routeChangeSuccess', function () {
+
+                var path = $location.path().substr(1); // Remove beginning /
+                var pane;
+                angular.forEach(panes, function(pos_pane) {
+                    if(pos_pane.title === path) {
+                        pane = pos_pane;
+                    }
+                });
+
+                $scope.select(pane);
+            });
+		},
+		template:
+			'<div class="tabbable">' +
+			'<ul class="nav nav-tabs">' +
+			'<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
+			'<a href="#/{{pane.title}}" ng-click="select(pane)">{{pane.title}}</a>' +
+			'</li>' +
+			'</ul>' +
+			'<div class="tab-content" ng-transclude></div>' +
+			'</div>',
+			replace: true
+	};
+}).
+directive('pane', function() {
+	return {
+		require: '^tabs',
+		restrict: 'E',
+		transclude: true,
+		scope: { title: '@' },
+		link: function(scope, element, attrs, tabsCtrl) {
+			tabsCtrl.addPane(scope);
+		},
+		template:
+			'<div class="tab-pane" ng-class="{active: selected}">' +
+			'</div>',
+	    replace: true
+	};
+});
